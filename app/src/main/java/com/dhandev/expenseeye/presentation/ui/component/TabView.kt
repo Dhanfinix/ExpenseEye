1d77a67793ab1df0d9131ff1235b7597f9d30195
Ramdhan <muhammad.ramdhan@sg-dsa.com>
2023-11-14 13:11:38 +0700

    ENHANCE(UI): Tab color

--- app/src/main/java/com/dhandev/expenseeye/presentation/ui/component/TabView.kt
+++ app/src/main/java/com/dhandev/expenseeye/presentation/ui/component/TabView.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dhandev.expenseeye.presentation.create.OutcomeScreen
+import com.dhandev.expenseeye.ui.theme.BlueMain
 import com.dhandev.expenseeye.ui.theme.ExpenseEyeTheme
 
 @Composable
@@ -23,10 +26,18 @@ fun TabView() {
     var selectedTabIndex by remember { mutableIntStateOf(0) }
 
     Column {
-        TabRow(selectedTabIndex = selectedTabIndex) {
+        TabRow(
+            selectedTabIndex = selectedTabIndex,
+            indicator = {tabPositions->
+                TabRowDefaults.Indicator(
+                    Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex]),
+                    color = BlueMain
+                )
+            }
+            ) {
             titles.forEachIndexed { index, title ->
                 Tab(
-                    text = { Text(title) },
+                    text = { Text(title, color = BlueMain) },
                     selected = selectedTabIndex == index,
                     onClick = { selectedTabIndex = index }
                 )
