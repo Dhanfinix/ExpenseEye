a8b99c90d610a9bc971344c7e9f7f8f6e169cb7f
Ramdhan <muhammad.ramdhan@sg-dsa.com>
2024-01-02 17:14:03 +0700

    ENHANCE(): use number format based on user locale for better performance

--- app/src/main/java/com/dhandev/rekapin/utils/NumUtil.kt
+++ app/src/main/java/com/dhandev/rekapin/utils/NumUtil.kt
@@ -1,6 +1,8 @@
 package com.dhandev.rekapin.utils
 
 import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.Locale
 
 object NumUtil {
     fun formatDecimal(n: Double): String? {
@@ -20,14 +22,21 @@ object NumUtil {
         }
 
     fun Double.formatThousand(): String {
-        val decimalFormatter = DecimalFormat("#,###")
-        return decimalFormatter.format(this)
+        val formatter = NumberFormat.getInstance(Locale.getDefault())
+        return formatter.format(this)
     }
 
     fun String.clearThousandFormat(): String {
-        return this.replace(",", "")
+        // Use the parse() method to convert the string to a number
+        val formatter = NumberFormat.getInstance(Locale.getDefault())
+        val number = formatter.parse(this)
+        // Use the toString() method to convert the number to a string
+        return number.toString()
     }
     fun String.clearDot(): String {
-        return this.replace(".0", "")
+        // Use the same logic as the clearThousandFormat() function
+        val formatter = NumberFormat.getInstance(Locale.getDefault())
+        val number = formatter.parse(this)
+        return number.toString()
     }
 }
